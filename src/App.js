3cc8e8bf0195141760a0dc844dd63ad515e8d921
Rochie424 <67237482+Rochie424@users.noreply.github.com>
2021-03-21 12:54:53 +0100

    Setting up panning

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
-import React, { Component } from "react";
-import Leaflet from "leaflet";
+import React, {useState, useEffect} from "react";
 import './App.css';
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import "tachyons";
 
 
 export default function App() {
+
+  const [coordinates, setCoordinates] = useState([8.82259, -2.8125]);
+
+  const moveISS = () => {
+    fetch('http://localhost:3001/iss-now', {
+      method: 'get',
+      headers: {'Content-Type': 'application/json'},
+    }) 
+    .then(response => response.json())
+    .then(data => {
+      let lat = Number(data['iss_position']['latitude']);
+      let lon = Number(data['iss_position']['longitude']);
+      setCoordinates([lat, lon]);
+    })
+  }
+
+  useEffect(() => { 
+    setInterval(moveISS, 10000);
+  }, []);
+
+  function Panning() {
+    const map = useMap();
+    map.panTo(coordinates);
+    return null;
+  }
+
   return (
     <div className="tc container"
      style={{display: "block"}}>
-    <h1>Tracking the ISS!</h1>
+    <h1>Tracking the ISS!<br/>{coordinates[0]}, {coordinates[1]}</h1>
     <MapContainer 
     id="map-container" 
-    center={[51.505, -0.09]} 
-    zoom={13} 
+    center={[8.82259, -2.8125]}
+    zoom={2} 
     scrollWheelZoom={false}>
+      <Panning/>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={coordinates}>
         <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
+          ISS
         </Popup>
       </Marker>
     </MapContainer>
